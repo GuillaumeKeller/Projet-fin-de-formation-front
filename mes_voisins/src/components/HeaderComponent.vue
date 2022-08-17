234bd248a5bedf7d21def64093ba8b492d7db8ea
JoffreyMS <joffrey.meistermann@gmail.com>
2022-08-17 15:49:52 +0200

    Responsive Mobile First Footer Component

--- mes_voisins/src/components/HeaderComponent.vue
+++ mes_voisins/src/components/HeaderComponent.vue
@@ -8,7 +8,7 @@
       <font-awesome-icon icon="fa-solid fa-magnifying-glass" />
       <input type="text" placeholder="Rechercher" />
     </div>
-    <div>
+    <div class="btn">
       <button href="#">Inscription</button>
       <button href="#">Connexion</button>
     </div>
@@ -19,6 +19,7 @@
 
 <style lang="scss" scoped>
   @import "@/assets/scss/variables.scss";
+  @import "@/assets/scss/media_queries.scss";
 
   header {
     display: flex;
@@ -41,11 +42,12 @@
     .fa-magnifying-glass {
       margin-right: 10px;
       height: 1.5em;
+      vertical-align: middle;
     }
 
-    input{
-      width: 300px;
-      height: 30px;
+    input {
+      width: 15rem;
+      height: 2rem;
       border-radius: 5px;
       border: 1px solid $maincolor;
       padding: 5px;
@@ -54,14 +56,47 @@
     }
 
     button {
-      background-color: $tertiarycolor;
+      background-color: $quaternarycolor;
       color: $fontcolor;
+      font-weight: bold;
       border: none;
-      padding: 10px;
+      padding: 0.6rem;
       border-radius: 5px;
       font-size: 1.2rem;
       cursor: pointer;
-      margin: 10px;
+      margin: 0.5rem;
+      box-shadow: 0.1em 0.1em 0.1em $tertiarycolor;
+    }
+
+    button:hover {
+      color: $tertiarycolor;
     }
   }
+
+  @media (max-width: $mediaSmartphone) {
+    header {
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      padding: 0.5em;
+      h1 {
+        font-size: 1.4rem;
+      }
+      .fa-people-carry-box {
+        font-size: 3em;
+      }
+      .search {
+        justify-content: center;
+        padding: 0.5em;
+        input {
+          width: 80%;
+        }
+      }
+
+      .btn {
+        width: 100%;
+      }
+    }
+  }
+
 </style>
