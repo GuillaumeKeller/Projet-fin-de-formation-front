3432fd6d3d6b94b4d7debf263a93d3f3ca650448
LeboulangerSimon <leboulanger.simon@gmail.com>
2022-08-24 15:17:24 +0200

    adcomponent not finished

---
+++ mes_voisins/src/components/AdComponent.vue
@@ -0,0 +1,33 @@
+<template>
+<article>
+    <div>
+        <h2 v-html="title"></h2>
+        <p v-html="description"></p>
+        <img v-html="img" alt="image de l'annonce">
+        <div v-html="category"></div>
+        <div v-html="type"></div>
+
+
+
+    </div>
+</article>
+</template>
+
+<script>
+export default
+{
+    props:
+    {
+        title: String,
+        desc: String,
+        img: String,
+        category: String,
+        type: String,
+
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
+
