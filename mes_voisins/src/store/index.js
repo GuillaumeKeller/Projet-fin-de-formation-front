6e405381ac3cfacce3f2753bb53bd522ec0ec0ff
GuillaumeKeller <guillaumekeller05@gmail.com>
2022-08-26 14:54:28 +0200

    Api request Login

--- mes_voisins/src/store/index.js
+++ mes_voisins/src/store/index.js
@@ -1,14 +1,35 @@
 import { createStore } from 'vuex'
+import storage from '@/utils/storage'
 
 export default createStore({
   state: {
+    isConnected: false
   },
   getters: {
+
   },
-  mutations: {
+  mutations: 
+  {
+    setConnected(state, connected) 
+    {
+    
+      state.isConnected = connected;
+    }
   },
-  actions: {
+  actions: 
+  {
+    userConnected(context)
+    {
+      context.commit( "setConnected", true );
+    },
+
+    userDisconnected(context)
+    {
+      context.commit( "setConnected", false );
+      storage.unset( "userData" );
+    }
   },
   modules: {
+
   }
 })
--- mes_voisins/src/views/LoginView.vue
+++ mes_voisins/src/views/LoginView.vue
@@ -1,148 +1,223 @@
 <template>
   <section>
-    <form @submit.prevent="login">
+    <form @submit.prevent="this.handleFormSubmit">
       <div class="form-title">
         <h2>Connexion</h2>
       </div>
       <div class="form-group">
         <label for="email">Email</label>
-        <input type="email" id="email" v-model="email" placeholder="Email" />
-        <div class="error">Vous devez saisir un identifiant !</div>
+        <input type="text" name="email" v-model="this.login" placeholder="john.doe@example.com" />
+        <div class="error" v-if="this.errors.loginEmpty">Vous devez saisir un identifiant !</div>
       </div>
       <div class="form-group">
         <label for="password">Mot de passe</label>
-        <input type="password" id="password" v-model="password" placeholder="Mot de passe" />
-        <div class="error">Vous devez saisir un mot de passe !</div>
+        <input
+          type="password"
+          id="password"
+          v-model="this.password"
+          placeholder="Mot de passe"
+        />
+        <div class="error" v-if="this.errors.passwordEmpty">Vous devez saisir un mot de passe !</div>
       </div>
-      <div class="error" style="font-weight: bold; margin-bottom: 1em; text-align: center">Identifiants incorrects</div>
+      <div
+        class="error"
+        v-if="this.errors.loginFailed"
+        style="font-weight: bold; margin-bottom: 1em; text-align: center"
+      >
+        Identifiants incorrects
+      </div>
       <div class="form-group">
         <button type="submit" class="form-button">Se connecter</button>
+      </div>
+      <div>
+        <p>
+      Pas encore inscrit ?
+      <router-link :to="{ name: 'register' }">Créer un compte </router-link>
+    </p>
       </div>
     </form>
   </section>
 </template>
 
-<script></script>
+<script>
+import userService from "@/services/userService";
+import storage from "@/utils/storage";
+
+export default {
+  name: "LoginView",
+
+  data() {
+    return {
+      login: "",
+      password: "",
+      errors: {
+        loginEmpty: false,
+        passwordEmpty: false,
+        loginFailed: false,
+      },
+    };
+  },
+
+  methods: {
+    async handleFormSubmit(evt) {
+      console.log("Form envoyé !", evt);
+
+      this.errors.loginFailed = false;
+
+      this.errors.loginEmpty = this.login == "";
+      this.errors.passwordEmpty = this.password == "";
+
+      if (!this.errors.loginEmpty && !this.errors.passwordEmpty) {
+        let data = await userService.login(this.login, this.password);
+
+        storage.set("userData", data);
+
+        if (await userService.isConnected()) {
+          this.$store.dispatch("userConnected");
+
+          this.$router.push({ name: "home" });
+        } else {
+          this.errors.loginFailed = true;
+        }
+      }
+    },
+  },
+};
+</script>
 
 <style lang="scss" scoped>
-  @import "@/assets/scss/variables.scss";
+@import "@/assets/scss/variables.scss";
 
-  section {
-    min-height: 100vh;
+section {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 80%;
+  margin: 0 auto;
+  background: $tertiaryColor;
+  color: $primaryColor;
+
+  h1 {
+    font-size: 1.5em;
+    color: $tertiaryColor;
+    font-weight: bold;
+    margin-bottom: 0.5em;
+  }
+
+  form {
     display: flex;
     flex-direction: column;
-    justify-content: center;
     align-items: center;
-    width: 80%;
-    margin: 0 auto;
-    background: $tertiaryColor;
-    color: $primaryColor;
+    width: 50%;
 
-    h1 {
-      font-size: 1.5em;
-      color: $tertiaryColor;
+    .form-title {
+      padding: 0.5em;
+      font-size: 1.1em;
       font-weight: bold;
-      margin-bottom: 0.5em;
+      margin-top: 1em;
+
+      h2 {
+        font-size: 1.5em;
+        color: $primaryColor;
+        font-weight: bold;
+        text-transform: uppercase;
+        background: linear-gradient(
+            to top,
+            #30dd8a,
+            #30dd8a 0.5em,
+            transparent 0.5em
+          )
+          no-repeat;
+      }
     }
 
-    form {
+    .form-group {
       display: flex;
       flex-direction: column;
       align-items: center;
-      width: 50%;
+      justify-content: center;
+      width: 80%;
+    }
 
-      .form-title {
-        padding: 0.5em;
-        font-size: 1.1em;
-        font-weight: bold;
-        margin-top: 1em;
+    .form-group input {
+      width: 100%;
+      height: 2.5rem;
+      border: double 2px transparent;
+      border-radius: 50px;
+      background-image: linear-gradient(white, white),
+        linear-gradient(to right, #30dd8a, #0ba360);
+      background-origin: border-box;
+      background-clip: padding-box, border-box;
+      font-size: 1.1em;
+      color: $primaryColor;
 
-        h2 {
-          font-size: 1.5em;
-          color: $primaryColor;
-          font-weight: bold;
-          text-transform: uppercase;
-          background: linear-gradient(to top, #30dd8a, #30dd8a 0.5em, transparent 0.5em) no-repeat;
-        }
-      }
-
-      .form-group {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        width: 80%;
-      }
-
-      .form-group input {
-        width: 100%;
-        height: 2.5rem;
+      &:focus {
+        outline: none;
         border: double 2px transparent;
         border-radius: 50px;
-        background-image: linear-gradient(white, white), linear-gradient(to right, #30dd8a, #0ba360);
-        background-origin: border-box;
-        background-clip: padding-box, border-box;
-        font-size: 1.1em;
-        color: $primaryColor;
-
-        &:focus {
-          outline: none;
-          border: double 2px transparent;
-          border-radius: 50px;
-          background-image: linear-gradient(white, white), linear-gradient(to right, #0ba360, #30dd8a);
-        }
-      }
-
-      .form-group label {
-        font-size: 1em;
-        color: $tertiaryColor;
-        margin-bottom: 0.5em;
-      }
-
-      button {
-        width: 12.5em;
-        font-size: 1em;
-        font-weight: bold;
-        color: #fff;
-        cursor: pointer;
-        margin: 0.9375em;
-        height: 3em;
-        text-align: center;
-        border: none;
-        background-size: 300% 100%;
-
-        border-radius: 50px;
-        -o-transition: all 0.4s ease-in-out;
-        -webkit-transition: all 0.4s ease-in-out;
-        transition: all 0.4s ease-in-out;
-      }
-
-      button:hover {
-        background-position: 100% 0;
-        -o-transition: all 0.4s ease-in-out;
-        -webkit-transition: all 0.4s ease-in-out;
-        transition: all 0.4s ease-in-out;
-      }
-
-      button:focus {
-        outline: none;
-      }
-
-      span {
-        margin-left: 0.2em;
-      }
-
-      .form-button {
-        background-image: linear-gradient(to right, #0ba360, #3cba92, #30dd8a, #2bb673);
-        box-shadow: 0 4px 15px 0 rgba(23, 168, 108, 0.75);
+        background-image: linear-gradient(white, white),
+          linear-gradient(to right, #0ba360, #30dd8a);
       }
     }
 
-    .error {
-      font-size: 0.8em;
-      color: #f00;
+    .form-group label {
+      font-size: 1em;
+      color: $tertiaryColor;
+      margin-bottom: 0.5em;
+    }
+
+    button {
+      width: 12.5em;
+      font-size: 1em;
+      font-weight: bold;
+      color: #fff;
+      cursor: pointer;
+      margin: 0.9375em;
+      height: 3em;
       text-align: center;
-      margin-top: 0.5em;
+      border: none;
+      background-size: 300% 100%;
+
+      border-radius: 50px;
+      -o-transition: all 0.4s ease-in-out;
+      -webkit-transition: all 0.4s ease-in-out;
+      transition: all 0.4s ease-in-out;
+    }
+
+    button:hover {
+      background-position: 100% 0;
+      -o-transition: all 0.4s ease-in-out;
+      -webkit-transition: all 0.4s ease-in-out;
+      transition: all 0.4s ease-in-out;
+    }
+
+    button:focus {
+      outline: none;
+    }
+
+    span {
+      margin-left: 0.2em;
+    }
+
+    .form-button {
+      background-image: linear-gradient(
+        to right,
+        #0ba360,
+        #3cba92,
+        #30dd8a,
+        #2bb673
+      );
+      box-shadow: 0 4px 15px 0 rgba(23, 168, 108, 0.75);
     }
   }
+
+  .error {
+    font-size: 0.8em;
+    color: #f00;
+    text-align: center;
+    margin-top: 0.5em;
+  }
+}
 </style>
