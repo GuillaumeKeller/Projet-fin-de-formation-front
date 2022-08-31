efe820efd559ca5fb482585f4e906f080a38ca9f
GuillaumeKeller <guillaumekeller05@gmail.com>
2022-08-31 18:08:13 +0200

    filters ads

--- mes_voisins/src/views/AdsView.vue
+++ mes_voisins/src/views/AdsView.vue
@@ -1,45 +1,34 @@
 <template>
   <section class="container">
     <div class="scrolling_menu">
-      <nav>
-        <div class="container-nav">
-          <label for="mobile">Recherche <font-awesome-icon icon="fa-solid fa-chevron-down" /></label>
+      <nav menu>
+        
+        <select>
+          <option >Choisissez un Département
 
-          <input type="checkbox" id="mobile" role="button" />
-          <ul>
-            <li class="scrolling">
-              <a href="#">Départements &ensp;<font-awesome-icon icon="fa-solid fa-chevron-down" /></a>
-              <ul class="menu">
-                <li><a href="#">01-Ain</a></li>
-                <li><a href="#">02-Aisne</a></li>
-                <li><a href="#">03-Allier</a></li>
-                <li><a href="#">04-Alpes de Hautes-Provence</a></li>
-              </ul>
-            </li>
-            <li class="scrolling">
-              <a href="#">Villes &ensp;<font-awesome-icon icon="fa-solid fa-chevron-down" /></a>
-              <ul class="menu">
-                <li><a href="#">Bourg-en-Bresse</a></li>
-                <li><a href="#">Laon</a></li>
-                <li><a href="#">Moulins</a></li>
-                <li><a href="#">Digne-les-Bains</a></li>
-              </ul>
-            </li>
-            <li class="scrolling">
-              <a href="#">Catégories &ensp;<font-awesome-icon icon="fa-solid fa-chevron-down" /></a>
-              <ul class="menu">
-                <li><a href="#">Service</a></li>
-                <li><a href="#">Matériels</a></li>
-              </ul>
-            </li>
-            <div class="btn">
-              <button class="btn-nav" href="#">
+          </option>
+          <option
+          v-for="location in this.locations" :key="location.id" 
+          >{{ location.name }}</option>
+        </select>
+
+        
+        <select>
+          <option >Choisissez une Catégories
+
+          </option>
+          <option
+          v-for="category in this.categories" :key="category.id"
+          >{{ category.name }}
+        </option>
+        </select>
+        <!--<div class="btn">
+              <button class="btn-nav">
                 Recherche
                 <font-awesome-icon icon="fa-solid fa-magnifying-glass" />
               </button>
-            </div>
-          </ul>
-        </div>
+        </div>-->
+        
       </nav>
     </div>
 
@@ -52,141 +41,215 @@
         :desc="ad.excerpt.rendered"
         :type="this.getTypeName(ad['AdType'][0])"
         :category="this.getCategoryName(ad['AdCategory'][0])"
-        :location= "this.getLocationName(ad['AdLocation'][0])"
+        :location="this.getLocationName(ad['AdLocation'][0])"
         :img="this.getAdImage(ad)"
+
+        @category-selected="this.filterByCategory"
+        @location-selected="this.filterByLocation"
+
+        v-show="this.shouldDisplayAd( ad )"
       />
     </div>
   </section>
 </template>
 
 <script>
-  import AdComponent from "@/components/AdComponent.vue";
-  import adService from "@/services/adService";
-  export default {
-    name: "AdsView",
-    components: {
-      AdComponent,
+import AdComponent from "@/components/AdComponent.vue";
+import adService from "@/services/adService";
+export default {
+  name: "AdsView",
+  components: {
+    AdComponent,
+  },
+
+  data() {
+    return {
+      ads: [],
+      types: [],
+      categories: [],
+      locations: [],
+    };
+  },
+
+  async created() {
+    this.ads = await adService.loadAds();
+    this.types = await adService.loadTypes();
+    this.categories = await adService.loadAdCategories();
+    this.locations = await adService.loadLocations();
+    console.log(this.ads);
+    console.log(this.types);
+    console.log(this.categories);
+    console.log(this.locations);
+  },
+
+  methods: {
+    getTypeName(typeID) {
+      for (let type of this.types) {
+        // Si l'ID correspond
+        if (typeID == type.id) {
+          return type.name;
+        }
+      }
+
+      return null;
     },
 
-    data() {
-      return {
-        ads: [],
-        types: [],
-        categories: [],
-        locations:[],
-      };
+    getCategoryName(categoryID) {
+      for (let category of this.categories) {
+        // Si l'ID correspond
+        if (categoryID == category.id) {
+          return category.name;
+        }
+      }
+
+      return null;
     },
 
-    async created() {
-      this.ads = await adService.loadAds();
-      this.types = await adService.loadTypes();
-      this.categories = await adService.loadAdCategories();
-      this.locations = await adService.loadLocations();
-      console.log(this.ads);
-      console.log(this.types);
-      console.log(this.categories);
-      console.log(this.locations);
+    getLocationName(locationID) {
+      for (let AdLocation of this.locations) {
+        // Si l'ID correspond
+        if (locationID == AdLocation.id) {
+          return AdLocation.name;
+        }
+      }
+
+      return null;
     },
 
-    methods: {
-      getTypeName(typeID) {
-        for (let type of this.types) {
-          // Si l'ID correspond
-          if (typeID == type.id) {
-            return type.name;
-          }
-        }
-
-        return null;
-      },
-
-      getCategoryName(categoryID) {
-        for (let category of this.categories) {
-          // Si l'ID correspond
-          if (categoryID == category.id) {
-            return category.name;
-          }
-        }
-
-        return null;
-      },
-
-      getLocationName(locationID) {
-        for (let AdLocation of this.locations) {
-          // Si l'ID correspond
-          if (locationID == AdLocation.id) {
-            return AdLocation.name;
-          }
-        }
-
-        return null;
-      },
-
-      getAdImage(ad) {
-        // Si la recette possède une image associée
-        if (ad.featured_media > 0) {
-          // Je renvoi l'URL de l'image
-          console.log(ad._embedded["wp:featuredmedia"][0].source_url);
-          return ad._embedded["wp:featuredmedia"][0].source_url;
-        } else {
-          return require("@/assets/img/no-image.jpg");
-        }
-      },
-    },
-
-    getAdImage(ad) 
-    {
-      // Si l'annonce possède une image associée
-      if( ad.featured_media > 0 )
-      {
+    getAdImage(ad) {
+      // Si la recette possède une image associée
+      if (ad.featured_media > 0) {
         // Je renvoi l'URL de l'image
-        console.log(ad._embedded['wp:featuredmedia'][0].source_url);
-        return ad._embedded['wp:featuredmedia'][0].source_url;
-        
+        console.log(ad._embedded["wp:featuredmedia"][0].source_url);
+        return ad._embedded["wp:featuredmedia"][0].source_url;
+      } else {
+        return require("@/assets/img/no-image.jpg");
       }
-      else
+    },
+
+    filterByLocation( location )
+    {
+      // Je stocke la difficulté cliquée dans le filtre du composant
+      this.filters.location = location;
+
+      // On réinitialise le filtre de type
+      this.filters.categorys = false;
+    },
+
+    filterByCategory( category )
+    {
+      // Je stocke le type de recette cliqué dans le filtre du composant
+      this.filters.categorys = category;
+
+      // On réinitialise le filtre de difficulté
+      this.filters.location = false;
+    },
+
+    shouldDisplayAd( p_ad )
+    {
+      
+      if( this.filters.location == false && this.filters.categorys == false )
       {
-        // Je renvoi une image générique ou d'erreur
-        // Ici, deux solutions https://cli.vuejs.org/guide/html-and-static-assets.html#static-assets-handling
-        // - Je renvoi un lien dynamique vers le fichier buildé (dans assets/img)
-        // return require("@/assets/img/unavailable_img.jpg" );
-        
-        // - Je renvoi l'image depuis le dossier public directement
-        // return "./unavailable_img.jpg";
+        return true;
       }
+
+      
+      if( this.filters.location != false )
+      {
+        
+        return this.getLocationName( p_ad.location[0] ) == this.filters.location
+      }
+
+      
+      if( this.filters.categorys != false )
+      {
+        
+        return this.getCategoryName( p_ad['AdCategory'][0] ) == this.filters.categorys
+      }
+
     }
+  },
 
-}
-
+  getAdImage(ad) {
+    // Si l'annonce possède une image associée
+    if (ad.featured_media > 0) {
+      // Je renvoi l'URL de l'image
+      console.log(ad._embedded["wp:featuredmedia"][0].source_url);
+      return ad._embedded["wp:featuredmedia"][0].source_url;
+    } else {
+      // Je renvoi une image générique ou d'erreur
+      // Ici, deux solutions https://cli.vuejs.org/guide/html-and-static-assets.html#static-assets-handling
+      // - Je renvoi un lien dynamique vers le fichier buildé (dans assets/img)
+      // return require("@/assets/img/unavailable_img.jpg" );
+      // - Je renvoi l'image depuis le dossier public directement
+      // return "./unavailable_img.jpg";
+    }
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-  // Import des variables du site
-  @import "@/assets/scss/variables.scss";
-  @import "@/assets/scss/media_queries.scss";
+// Import des variables du site
+@import "@/assets/scss/variables.scss";
+@import "@/assets/scss/media_queries.scss";
 
-  // Import de la police du site
-  @import url("https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
+// Import de la police du site
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
 
-  .container {
-    min-height: 100vh;
+.container {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  width: 80%;
+  margin: 0 auto;
+  background: $tertiaryColor;
+  color: $primaryColor;
+}
+
+// ----------------------------------- Navigation menu tablet et phone----------------------------------------
+
+nav {
+  width: 100%;
+  margin: 0 auto;
+  
+  
+}
+
+label{
     display: flex;
     flex-direction: column;
-    width: 80%;
-    margin: 0 auto;
-    background: $tertiaryColor;
+    align-items: center;
+    margin-bottom: 1em;
+    margin-top: 1em;
+    color: black;
+}
+
+select{
+    width: 30%;
+        height: 2.5rem;
+        border: double 2px transparent;
+        border-radius: 50px;
+        background-image: linear-gradient(white, white), linear-gradient(to right, #30dd8a, #0ba360);
+        background-origin: border-box;
+        background-clip: padding-box, border-box;
+        font-size: 1.1em;
+        color: $primaryColor;
+        text-align: center;
+}
+
+input {
+    width: 30%;
+    height: 2.5rem;
+    border: double 2px transparent;
+    border-radius: 50px;
+    background-image: linear-gradient(white, white), linear-gradient(to right, #30dd8a, #0ba360);
+    background-origin: border-box;
+    background-clip: padding-box, border-box;
+    font-size: 1.1em;
     color: $primaryColor;
-  }
-
-  // ----------------------------------- Navigation menu tablet et phone----------------------------------------
-
-  nav {
-    width: 100%;
-    margin: 0 auto;
-    background-color: $tertiaryColor;
-    position: sticky;
-    top: 0px;
-  }
+    text-align: center;
+}
+/*
   nav input[type="checkbox"] {
     display: none;
   }
@@ -349,27 +412,29 @@
     }
   }
 
-  // ----------------------------------- Ads ----------------------------------------
+  */
 
+// ----------------------------------- Ads ----------------------------------------
+
+.ads {
+  display: flex;
+  width: 80%;
+  justify-content: space-around;
+  margin: 0 auto;
+  margin-bottom: 2em;
+  flex-direction: row;
+  flex-wrap: wrap;
+  box-shadow: 0px 0px 0px 2px #30dd8a;
+  border-radius: 1em;
+}
+
+@media screen and (max-width: $mediaTablet) {
+  .container {
+    width: 100%;
+  }
   .ads {
-    display: flex;
     width: 80%;
-    justify-content: space-around;
-    margin: 0 auto;
-    margin-bottom: 2em;
-    flex-direction: row;
-    flex-wrap: wrap;
-    box-shadow: 0px 0px 0px 2px #30dd8a;
-    border-radius: 1em;
+    padding: 1em;
   }
-
-  @media screen and (max-width: $mediaTablet) {
-    .container {
-      width: 100%;
-    }
-    .ads {
-      width: 80%;
-      padding: 1em;
-    }
-  }
+}
 </style>
