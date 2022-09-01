50dae1cd84becbe2d24aa2d035f753c99545f86d
LeboulangerSimon <leboulanger.simon@gmail.com>
2022-09-01 16:33:06 +0200

    pagination works

--- mes_voisins/src/router/index.js
+++ mes_voisins/src/router/index.js
@@ -57,7 +57,7 @@ const routes = [
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes
+  routes,
+  
 })
-
 export default router
--- mes_voisins/src/services/adService.js
+++ mes_voisins/src/services/adService.js
@@ -3,83 +3,84 @@ import storage from '@/utils/storage';
 
 // Module nous permettant d'interagir avec l'API de notre Backend Mes-voisins
 const adService = {
-
+  
   // Propriété qui stocke la BASE URL de notre API
   base_url: "http://joffreyms-server.eddi.cloud/back/projet-mes-voisins-back/public/wp-json",
-
-
+  
+  
   // Méthode qui charge les annonces du site
   async loadAds() {
     const response = await axios.get(this.base_url + "/wp/v2/ad?_embed=true");
-
+    
     return response.data;
   },
+  
   async loadAdCategories() {
     const response = await axios.get(this.base_url + "/wp/v2/adcategory");
     return response.data;
   },
-
+  
   async loadTypes() {
     const response = await axios.get(this.base_url + "/wp/v2/adtype");
     return response.data;
   },
-
+  
   async loadLocations() {
     const response = await axios.get(this.base_url + "/wp/v2/adlocation");
     return response.data;
   },  
-
+  
   async loadAd(ad_id) {
     const response = await axios.get(this.base_url + "/wp/v2/ad/" + ad_id + "?_embed=true");
     return response.data;
   },
-
+  
   async loadAdCategory(category_id) {
     const response = await axios.get(this.base_url + "/wp/v2/adcategory?post=" + category_id);
     return response.data;
   },
-
+  
   async loadAdType(type_id) {
     const response = await axios.get(this.base_url + "/wp/v2/adtype?post=" + type_id);
     return response.data;
   },
-
+  
   async loadAdStatus(status_id) {
     const response = await axios.get(this.base_url + "/wp/v2/adstatus?post=" + status_id);
     return response.data;
   },
-
+  
   async loadLocation(location_id) {
     const response = await axios.get(this.base_url + "/wp/v2/adlocation?post="+ location_id);
     return response.data;
   },  
-
+  
   async loadAdImage(media_id) {
     const response = await axios.get(this.base_url + "/wp/v2/media/" + media_id);
     return response.data;
   },
-
+  
   async loadUserData(user_id) {
     const response = await axios.get(this.base_url + "/mesvoisins/v1/userdata/" + user_id);
     return response.data;
   },
-
+  
   async loadUser(user_id)
   {
     const response = await axios.get(this.base_url+ "/wp/v2/users/" + user_id);
     return response.data;
-
+    
   },
-
+  
   async loadAdAuthor(user_id)
   {
     const response = await axios.get(this.base_url+ "/wp/v2/ad?author=" + user_id );
     return response.data;
   },
-
-
-
-
+  
+  
+  
+  
   async addComment(msg, ad_id) {
     const response = await axios.post(
       this.base_url + "/wp/v2/comments", {
@@ -90,48 +91,48 @@ const adService = {
           Authorization: "Bearer " + storage.get("userData").token
         }
       }
-    );
-    return response.data;
-  },
-
-  async addAd( ad )
-  {
-    const response = await axios.post( 
-      this.base_url + "/wp/v2/ad", 
-      {
-        title: ad.title,
-        content: ad.text,
-        adexcerpt: ad.text,
-        AdType: ad.type,
-        AdCategory: ad.category, 
-        AdLocation: ad.location,
-        status: "publish"
-                  
+      );
+      return response.data;
+    },
+    
+    async addAd( ad )
+    {
+      const response = await axios.post( 
+        this.base_url + "/wp/v2/ad", 
+        {
+          title: ad.title,
+          content: ad.text,
+          adexcerpt: ad.text,
+          AdType: ad.type,
+          AdCategory: ad.category, 
+          AdLocation: ad.location,
+          status: "publish"
+          
+        }, 
+        {
+          
+          headers: {
+            Authorization: "Bearer " + storage.get("userData").token
+          }
+        }
+        );
+        return response.data;
       }, 
-      {
       
-      headers: {
-        Authorization: "Bearer " + storage.get("userData").token
+      uploadImage( file )
+      {
+        const formData = new FormData();
+        formData.append( "file", file );
+        return axios.post( this.base_url + "/wp/v2/media", formData, {
+          headers: {
+            Authorization: "Bearer " + storage.get("userData").token
+          }
+        } );
       }
-    }
-    );
-    return response.data;
-  }, 
-
-  uploadImage( file )
-  {
-    const formData = new FormData();
-    formData.append( "file", file );
-    return axios.post( this.base_url + "/wp/v2/media", formData, {
-      headers: {
-        Authorization: "Bearer " + storage.get("userData").token
-      }
-    } );
-  }
-  
-
-};
-
-// Pour pouvoir utiliser le service dans d'autres composants
-// il faut export le module (et l'import de l'autre coté)
-export default adService;
+      
+      
+    };
+    
+    // Pour pouvoir utiliser le service dans d'autres composants
+    // il faut export le module (et l'import de l'autre coté)
+    export default adService;
