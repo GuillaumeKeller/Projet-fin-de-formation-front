8df4bf79dde12f46190b31b1e898707f101f462f
GuillaumeKeller <guillaumekeller05@gmail.com>
2022-09-02 16:29:09 +0200

    css edit

--- mes_voisins/src/views/ProfileView.vue
+++ mes_voisins/src/views/ProfileView.vue
@@ -1,169 +1,174 @@
 <template>
-  <div class="profil_page">
-    <div class="profile">
-      <div class="infos">
-        <img class="profile_picture" :src="this.user.source_url" alt=" Image de l'utilisateur" />
-        <ul>
-          <li><span class="ad__info">Pseudo: <p v-html="this.user.slug"></p></span></li>
-          <li><span class="ad__info">Email : <p v-html="this.userdata[0]['email']"></p> </span></li>
-          <li><span class="ad__info">Tel : <p v-html="this.userdata[0]['phone']"></p></span></li>
-        </ul>
-      </div>
+  <div class="profile_page">
+    <div class="profile_infos">
+      <!-- <img class="profile_picture" :src="this.user.source_url" alt=" Image de l'utilisateur" /> -->
+      <h1>Coordonnées</h1>
+      <ul>
+        <li>
+          <span class="ad__info"
+            >Pseudo:
+            <p v-html="this.user.slug"></p
+          ></span>
+        </li>
+        <li>
+          <span class="ad__info">
+            Email :
+            <p v-html="this.userdata[0]['email']"></p>
+          </span>
+        </li>
+        <li>
+          <span class="ad__info">
+            Téléphone :
+            <p v-html="this.userdata[0]['phone']"></p
+          ></span>
+        </li>
+      </ul>
     </div>
-  </div>
+
     <section>
       <div class="ads">
         <h1>Annonces</h1>
 
         <div class="ad">
-
           <AdComponent
-         v-for="authorAds in this.author"
-          :key="authorAds.id"
-          :dbid="authorAds.id"
-          :title="authorAds.title.rendered"
-          :desc="authorAds.excerpt.rendered"
-          
+            v-for="authorAds in this.author"
+            :key="authorAds.id"
+            :dbid="authorAds.id"
+            :title="authorAds.title.rendered"
+            :desc="authorAds.excerpt.rendered"
           />
-
-          
         </div>
       </div>
     </section>
+  </div>
 </template>
 
 <script>
-  import adService    from "@/services/adService";
-  import AdComponent  from "@/components/AdComponent.vue";
+import adService from "@/services/adService";
+import AdComponent from "@/components/AdComponent.vue";
 
-  export default{
+export default {
+  components: {
+    AdComponent,
+  },
 
-    components:
-    {
-      AdComponent,
-    },
-  
-
-    async created(){
-  
+  async created() {
     this.userdata = await adService.loadUserData(this.$route.params.id);
-    this.ads      = await adService.loadAds()
-    this.user     = await adService.loadUser(this.$route.params.id);
-    this.author   = await adService.loadAdAuthor(this.$route.params.id)
-    
-    console.log('coucou');
-    console.log(this.author[0].id);
-  
-  
-    },
+    this.ads = await adService.loadAds();
+    this.user = await adService.loadUser(this.$route.params.id);
+    this.author = await adService.loadAdAuthor(this.$route.params.id);
 
-    data() {
-          return {
-            ads: false,
-            userdata: false,
-            user: false,
-            author: [],
-            img: false,
-          }
-        
-        },
-  
-  methods: 
-  {
-     getAdImage(ad) 
-    {
+    console.log("coucou");
+    console.log(this.author[0].id);
+  },
+
+  data() {
+    return {
+      ads: false,
+      userdata: false,
+      user: false,
+      author: [],
+      img: false,
+    };
+  },
+
+  methods: {
+    getAdImage(ad) {
       // Si l'annonce possède une image associée
-      if( ad.featured_media > 0 )
-      {
+      if (ad.featured_media > 0) {
         // Je renvoi l'URL de l'image
         return this.author[0]._links["wp:featuredmedia"][0].href;
-        
       }
     },
-  }
-}
-
- 
-
-
-
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 @import "@/assets/scss/variables.scss";
 @import "@/assets/scss/media_queries.scss";
-.profil_page {
+
+.profile_page {
   min-height: 100vh;
-}
-.infos {
   width: 80%;
-  margin-top: 2em;
-  margin-left: 10%;
-  margin-bottom: 3em;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  img {
-    display: flex;
-    width: 150px;
-    border-radius: 50%;
-    border: 2px solid black;
-  }
-  ul {
-    height: 80px;
-    width: 300px;
-    border: solid 2px black;
-    background-color: $tertiaryColor;
-    text-align: left;
-    padding: 10px 0 0 10px;
-  }
-  p {
-    height: 150px;
-    width: 500px;
-    border: solid 2px black;
-    background-color: $tertiaryColor;
-    text-align: left;
-    padding: 10px 0 0 10px;
-  }
+  margin: 0 auto;
+  background: $tertiaryColor;
+  color: $primaryColor;
 }
-section {
-  height: 100%;
+
+h1 {
+  font-family: "Roboto", sans-serif;
+  font-size: 1.5em;
+  line-height: 2em;
+  letter-spacing: 0.5em;
+  color: $primaryColor;
+  text-transform: uppercase;
+  font-weight: bold;
+  background: linear-gradient(to top, #30dd8a, #30dd8a 0.3em, transparent 0.3em)
+    no-repeat;
+}
+
+.profile_infos {
   width: 80%;
-  margin-left: 10%;
-  border: solid 2px black;
-  h1 {
-    font-size: 1.5em;
+  margin: 0 auto;
+  padding-top: 100px;
+  background: $tertiaryColor;
+  color: $primaryColor;
+}
+
+ul {
+  text-align: left;
+  margin-top: 20px;
+
+  li {
+    font-size: 1.2em;
+    line-height: 1.5em;
+    letter-spacing: 0.1em;
+    color: $primaryColor;
     font-weight: bold;
-    margin-bottom: 0.8em;
-    margin-left: 10%;
-    text-align: left;
-  }
-  .ads {
-    padding: 1.3em 0 1.3em 0;
-    background-color: $tertiaryColor;
-  }
-  .ad {
-    padding: 2em 0 2em 0;
-    border: solid 2px black;
-    width: 90%;
-    margin-left: 5%;
-    margin-bottom: 1em;
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    background-color: $backgroundColor;
-    img{
-        border: 2px solid black;
-        margin-left: 3%;
+
+    p {
+      font-weight: 400;
     }
   }
-  .ad__info {
-    p {
-      border: solid 2px black;
-      height: 100px;
-      padding: 10px 10px;
-      margin: 2em 2em ;
-      background-color: $tertiaryColor;
+}
+
+section {
+  margin-top: 2em;
+}
+.ads {
+  display: flex;
+  flex-direction: column;
+  width: 80%;
+  margin: 0 auto;
+  background: $tertiaryColor;
+  color: $primaryColor;
+
+  .ad {
+    margin-top: 20px;
+    border: solid 2px #30dd8a;
+    border-radius: 20px;
+    height: 400px;
+    overflow-x: hidden;
+    overflow-y: auto;
+  }
+}
+
+@media (max-width: $mediaTablet) {
+  .profile_page {
+    min-height: 100vh;
+    width: 100%;
+    margin: 0 auto;
+    background: $tertiaryColor;
+    color: $primaryColor;
+  }
+}
+
+@media (max-width: $mediaSmartphone) {
+  .ads {
+    .ad {
+      border: none;
+      overflow: hidden;
     }
   }
 }
